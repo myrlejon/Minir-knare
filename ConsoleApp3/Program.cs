af4ae9c49021211a3a557a5e9cde69e83557078a
Nils Odén <nisse757@hotmail.se>
2020-11-18 14:59:45 +0100

    Skapade fem kodblock, två för operatorer och tre för termer.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,32 @@ namespace Miniräknare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //Operatorer, här kan användaren skriva in +, -, *, / osv.
+            Console.WriteLine("Enter first operator: ");
+            string firstOperator = Console.ReadLine();
+            int firstOperatorInt = Convert.ToInt32(firstOperator);
+
+            Console.WriteLine("Enter second operator: ");
+            string secondOperator = Console.ReadLine();
+            int secondOperatorInt = Convert.ToInt32(secondOperator);
+            
+            //Termer, här kan användaren skriva in tal.
+            Console.WriteLine("Enter first term: ");
+            string firstTerm = Console.ReadLine();
+            int firstTermInt = Convert.ToInt32(firstTerm);
+
+            Console.WriteLine("Enter second term: ");
+            string secondTerm = Console.ReadLine();
+            int secondTermInt = Convert.ToInt32(secondTerm);
+
+            Console.WriteLine("Enter third term: ");
+            string thirdTerm = Console.ReadLine();
+            int thirdTermInt = Convert.ToInt32(thirdTerm);
+
+            if (firstOperatorInt = +) ;
+            {
+
+            }
         }
     }
 }
